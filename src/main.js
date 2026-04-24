7718413b5a1bf681394aee118b45d0f184a443c9
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-24 17:27:02 -0500

    feat(material-ui): add primevue ui component library and theme configuration.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Material from '@primeuix/themes/material';
 import './style.css'
 import App from './app.vue'
+import {Avatar, Button, Card, Drawer,
+    Image, Menu, Menubar, SelectButton,
+    Toolbar, Tooltip} from "primevue";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue, { ripple: true, theme: { preset: Material}})
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
+    .component('pv-card', Card)
+    .component('pv-image', Image)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-drawer', Drawer)
+    .directive('tooltip', Tooltip)
+    .mount('#app')
