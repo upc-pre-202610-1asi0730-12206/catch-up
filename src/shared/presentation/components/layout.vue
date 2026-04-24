519082eca2433b3f4005a853126e30534c8e1883
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-24 18:12:35 -0500

    feat(layout): add layout component with menubar, drawer toggle, and footer content integration.

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from "vue";
+import LanguageSwitcher from "./language-switcher.vue";
+import FooterContent from "./footer-content.vue";
+
+const drawerVisible = ref(false);
+const toggleDrawer = () => {
+  drawerVisible.value = !drawerVisible.value
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <pv-menubar>
+        <template #start>
+          <pv-button icon="pi pi-bars" @click="toggleDrawer" label="CatchUp" text/>
+          <!-- Add Source List here -->
+        </template>
+        <template #end>
+          <language-switcher/>
+        </template>
+      </pv-menubar>
+    </div>
+  </div>
+  <div>
+    <!-- Add Article List here -->
+    <!-- Unavailable Content Component here -->
+  </div>
+  <footer-content/>
+</template>
+
+<style scoped>
+
+</style>
