dbed51c2e4bdc4cae079618f51fa376a79ca989a
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-24 17:50:53 -0500

    feat(i18n): add language switcher component for i18n support.

---
+++ src/shared/presentation/components/language-switcher.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale"
+                    :options="$i18n.availableLocales">
+    <template #option="slotProps">
+      <span>{{ slotProps.option.toUpperCase() }}</span>
+    </template>
+  </pv-select-button>
+</template>
+
+<style scoped>
+
+</style>
